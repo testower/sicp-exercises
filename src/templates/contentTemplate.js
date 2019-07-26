4eb3cf5cf1de90cd2e12152731eefca34fded2eb
Tom Erik Støwer <testower@gmail.com>
2019-07-26 21:51:38 +0200

    Initial commit

---
+++ src/templates/contentTemplate.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <SEO title={frontmatter.title} />
+      <h1>{frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+`
