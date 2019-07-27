574719c41c427795c1d8169e68ae5a89cc00e44a
Tom Erik Støwer <testower@gmail.com>
2019-07-27 20:17:30 +0200

    Automate index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,40 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, {Fragment} from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h3>Chapter 1</h3>
-    <ul>
-      <li><Link to="/1-1/">1.1</Link></li>
-      <li><Link to="/1-2/">1.2</Link></li>
-    </ul>
+    {data.allMarkdownRemark.distinct.map(chapter =>
+      <Fragment>
+        <h2>Chapter {chapter}</h2>
+        <ul>
+          {data.allMarkdownRemark.edges.filter(edge => `${edge.node.frontmatter.chapter}` === chapter).map(edge =>
+            <li><Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link></li>
+          )}
+        </ul>
+      </Fragment>
+    )}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+query allNotesQuery {
+  allMarkdownRemark(sort: {fields: frontmatter___chapter, order: ASC}) {
+    edges {
+      node {
+        frontmatter {
+          chapter
+          path
+          title
+        }
+      }
+    }
+    distinct(field: frontmatter___chapter)
+  }
+}
+`
